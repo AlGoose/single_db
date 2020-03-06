e459ec72f7b51ed41bb86ca4fb46ed35d67cf07d
Александр Гусев <alex1232131@mail.ru>
2020-03-06 16:18:42 +0700

    add Timeline demo

---
+++ src/components/Timeline.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="1" class="test" @click="toTop">
+        <a id="back-top">&#9650; Наверх</a>
+      </v-col>
+      <v-col cols="11">
+        <v-timeline dense>
+          <v-timeline-item v-for="n in count" :key="n" small>
+            <v-card style="width: 98%">
+              <v-card-title class="headline">Lorem ipsum</v-card-title>
+              <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-card-subtitle>09:41 | 01.01.1980</v-card-subtitle>
+              </v-card-actions>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+        <v-btn color="indigo" block text @click="addNews">More</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    count: 5,
+    settings: {
+      maxScrollbarLength: 60
+    }
+  }),
+
+  methods: {
+    addNews() {
+      this.count += 5;
+    },
+
+    toTop() {
+      console.log("top");
+      this.$vuetify.goTo(0);
+    }
+  }
+};
+</script>
+
+<style>
+.test:hover {
+  background: rgba(225, 231, 237, 0.8);
+}
+
+#back-top {
+  position: fixed;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Timeline/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Timeline from "@/components/Timeline.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+    Timeline
   }
 };
 </script>
